33a307a92b3e674864e4ef7228ea159159f0ae95
verche_a <verche_a@etna-alternance.net>
2016-07-29 15:45:24 +0200

    Résolution via l'adresse IP publique

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -9,7 +9,7 @@ node_name               chef_user
 client_key              "#{File.dirname(__FILE__)}/#{chef_user}.pem"
 
 #chef_server_url         'https://chef.zone-flight.local/organizations/zone-flight/'
-chef_server_url         'https://172.16.16.18/organizations/zone-flight/nodes'
+chef_server_url         'https://163.5.245.201/organizations/zone-flight/'
 ssl_verify_mode         :verify_none
 
 cache_type              'BasicFile'
